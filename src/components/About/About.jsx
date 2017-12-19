64a66b50a5357015943f4d214b75a139e99df964
tom-workman <tom.workman@us.ibm.com>
2017-12-19 11:37:34 -0600

    Add Carbon to package & test Tile component in About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
-import Card from "react-md/lib/Cards/Card";
-import CardText from "react-md/lib/Cards/CardText";
+// import Card from "react-md/lib/Cards/Card";
+// import CardText from "react-md/lib/Cards/CardText";
+import { Tile } from "carbon-components-react/lib/components/Tile";
 import UserLinks from "../UserLinks/UserLinks";
 import config from "../../../data/SiteConfig";
 import "./About.scss";
+import "carbon-components/scss/components/tile/_tile.scss";
 
 class About extends Component {
   render() {
     return (
       <div className="about-container md-grid mobile-fix">
-        <Card className="md-grid md-cell--8">
-          <div className="about-wrapper">
-            <img
-              src={config.userAvatar}
-              className="about-img"
-              alt={config.userName}
-            />
-            <CardText>
-              <p className="about-text md-body-1">
-                {config.userDescription}
-              </p>
-            </CardText>
-            <UserLinks labeled config={config} />
-          </div>
-        </Card>
+				<Tile>
+					<div className="about-wrapper">
+						<img
+							src={config.userAvatar}
+							className="about-img"
+							alt={config.userName}
+						/>
+						<p className="about-text md-body-1">
+						Spicy jalapeno bacon ipsum dolor amet venison strip steak beef ribs, jowl turducken prosciutto drumstick porchetta buffalo doner biltong tri-tip beef corned beef. Cupim pork chop landjaeger, pork loin tenderloin pork kevin brisket doner drumstick. Landjaeger biltong buffalo shank short ribs. Jowl chicken rump pork loin ham hock, short ribs buffalo pig ball tip ham shank t-bone. Shankle strip steak swine, pancetta kevin corned beef sausage chicken pig pork loin pork t-bone turkey beef ribs.
+						</p>
+					</div>
+					<UserLinks labeled config={config} />
+				</Tile>
       </div>
     );
   }
