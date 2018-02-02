bdcce5c0fac87e884f4891da4346e9ae8beb63dc
tom-workman <tom.workman@us.ibm.com>
2018-02-02 10:25:06 -0600

    fixes issues for #20

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
-import { Tile } from "carbon-components-react/lib/components/Tile";
+import { Tile } from "carbon-components-react";
 import UserLinks from "../UserLinks/UserLinks";
 import config from "../../../data/SiteConfig";
 import "./About.scss";
-import "carbon-components/scss/components/tile/_tile.scss";
 
 class About extends Component {
-  render() {
+	render() {
     return (
       <div className="about-container md-grid mobile-fix">
-				<Tile>
-					<div className="about-wrapper">
-						<img
-							src={config.userAvatar}
-							className="about-img"
-							alt={config.userName}
-						/>
-						<p className="about-text md-body-1">
+        <Tile>
+          <div className="about-wrapper">
+            <img
+              src={config.userAvatar}
+              className="about-img"
+              alt={config.userName}
+            />
+            <p className="about-text md-body-1">
 						Spicy jalapeno bacon ipsum dolor amet venison strip steak beef ribs, jowl turducken prosciutto drumstick porchetta buffalo doner biltong tri-tip beef corned beef. Cupim pork chop landjaeger, pork loin tenderloin pork kevin brisket doner drumstick. Landjaeger biltong buffalo shank short ribs. Jowl chicken rump pork loin ham hock, short ribs buffalo pig ball tip ham shank t-bone. Shankle strip steak swine, pancetta kevin corned beef sausage chicken pig pork loin pork t-bone turkey beef ribs.
-						</p>
-					</div>
-					<UserLinks labeled config={config} />
-				</Tile>
+            </p>
+          </div>
+          <UserLinks labeled config={config} />
+        </Tile>
       </div>
     );
   }
