1632e176275c76d6398c5dc2e4c03d139d25602e
tom-workman <tom.workman@us.ibm.com>
2018-02-02 10:27:22 -0600

    Fix paragraph for issue #20

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,8 +15,7 @@ class About extends Component {
               className="about-img"
               alt={config.userName}
             />
-            <p className="about-text md-body-1">
-						Spicy jalapeno bacon ipsum dolor amet venison strip steak beef ribs, jowl turducken prosciutto drumstick porchetta buffalo doner biltong tri-tip beef corned beef. Cupim pork chop landjaeger, pork loin tenderloin pork kevin brisket doner drumstick. Landjaeger biltong buffalo shank short ribs. Jowl chicken rump pork loin ham hock, short ribs buffalo pig ball tip ham shank t-bone. Shankle strip steak swine, pancetta kevin corned beef sausage chicken pig pork loin pork t-bone turkey beef ribs.
+            <p className="about-text md-body-1">Spicy jalapeno bacon ipsum dolor amet venison strip steak beef ribs, jowl turducken prosciutto drumstick porchetta buffalo doner biltong tri-tip beef corned beef. Cupim pork chop landjaeger, pork loin tenderloin pork kevin brisket doner drumstick. Landjaeger biltong buffalo shank short ribs. Jowl chicken rump pork loin ham hock, short ribs buffalo pig ball tip ham shank t-bone. Shankle strip steak swine, pancetta kevin corned beef sausage chicken pig pork loin pork t-bone turkey beef ribs.
             </p>
           </div>
           <UserLinks labeled config={config} />
