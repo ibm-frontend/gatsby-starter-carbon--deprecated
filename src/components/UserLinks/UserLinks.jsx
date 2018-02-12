9abf2af6b2889585a03c546533bda2020693f87c
Diego Hernandez <Diego.Hernandez@ibm.com>
2018-02-12 12:18:28 -0600

    refactor(user-links): Refactor look and feel of user links component

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -1,32 +1,21 @@
 import React, { Component } from "react";
-import Button from "react-md/lib/Buttons";
+import { Link } from "carbon-components-react";
 import "./UserLinks.scss";
 
 class UserLinks extends Component {
-  getLinkElements() {
-    const { userLinks } = this.props.config;
-    const { labeled } = this.props;
-    return userLinks.map(link =>
-      <Button
-        icon={!labeled}
-        flat={labeled}
-        secondary
-        key={link.label}
-        iconClassName={link.iconClassName}
-        href={link.url}
-      >
-        {labeled ? link.label : ""}
-      </Button>
-    );
-  }
   render() {
+    const { labeled } = this.props;
     const { userLinks } = this.props.config;
     if (!userLinks) {
       return null;
     }
     return (
       <div className="user-links">
-        {this.getLinkElements()}
+        {userLinks.map(link =>
+          (<Link href={link.url} className="user-links__link">
+            <i className={link.iconClassName} /> {labeled ? link.label : ""}
+          </Link>)
+        )}
       </div>
     );
   }
