1b17f82d15eb449d9d6d1c53e88ea75f2e1ee474
Diego Hernandez <Diego.Hernandez@ibm.com>
2018-02-12 12:00:37 -0600

    refactor(footer): Make footer a functional component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import UserLinks from "../UserLinks/UserLinks";
 import config from "../../../data/SiteConfig";
 import "./Footer.scss";
 
-class Footer extends Component {
-  render() {
-    const { userLinks } = this.props;
-    const copyright = config.copyright;
-    const fixedFooter = config.fixedFooter;
-    if (!copyright) {
-      return null;
-    }
-    return (
-      <footer className={fixedFooter ? "footer footer--fixed" : "footer"}>
-        <div className="footer__container">
-          <div className="footer__left">
-            {copyright}
-          </div>
-          <div className="footer__right">
-            Based on{" "}
-            <a href="https://github.com/ibm-frontend/gatsby-starter-carbon">
-              Gatsby Carbon Starter
-            </a>.
-            {userLinks ? <UserLinks config={config} labeled /> : null}
-          </div>
-        </div>
-      </footer>
-    );
+const Footer = (props) => {
+  const { userLinks } = props;
+  const copyright = config.copyright;
+  const fixedFooter = config.fixedFooter;
+
+  if (!copyright) {
+    return null;
   }
+
+  return (
+    <footer className={fixedFooter ? "footer footer--fixed" : "footer"}>
+      <div className="footer__container">
+        <div className="footer__left">
+          {copyright}
+        </div>
+        <div className="footer__right">
+          Based on{" "}
+          <a href="https://github.com/ibm-frontend/gatsby-starter-carbon">
+            Gatsby Carbon Starter
+          </a>.
+          {userLinks ? <UserLinks config={config} labeled /> : null}
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
