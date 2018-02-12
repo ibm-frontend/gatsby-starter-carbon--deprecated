acd0743649b4d33fcc9ab2dbecd661ebebdbed70
Diego Hernandez <Diego.Hernandez@ibm.com>
2018-02-12 11:50:06 -0600

    refactor(footer): Update footer to match carbon's design system look and feel

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-import { Button } from 'carbon-components-react';
-import Link from "gatsby-link";
 import UserLinks from "../UserLinks/UserLinks";
 import config from "../../../data/SiteConfig";
 import "./Footer.scss";
 
 class Footer extends Component {
   render() {
-    const url = config.siteRss;
     const { userLinks } = this.props;
     const copyright = config.copyright;
     const fixedFooter = config.fixedFooter;
@@ -15,29 +12,17 @@ class Footer extends Component {
       return null;
     }
     return (
-      <footer className={fixedFooter ? "footer footer-fixed" : "footer"}>
-        {userLinks ? <UserLinks config={config} labeled /> : null}
-        <div className="notice-container">
-          <div className="copyright">
-            <h4>
-              {copyright}
-            </h4>
+      <footer className={fixedFooter ? "footer footer--fixed" : "footer"}>
+        <div className="footer__container">
+          <div className="footer__left">
+            {copyright}
           </div>
-
-          <div className="rss">
-            <Link to={url}>
-              <Button type="link">
-                Subscribe
-              </Button>
-            </Link>
-          </div>
-          <div className="based-on">
-            <h4>
-              Based on{" "}
-              <a href="https://github.com/ibm-frontend/gatsby-starter-carbon/">
-                Gatsby Carbon Starter
-              </a>.
-            </h4>
+          <div className="footer__right">
+            Based on{" "}
+            <a href="https://github.com/ibm-frontend/gatsby-starter-carbon">
+              Gatsby Carbon Starter
+            </a>.
+            {userLinks ? <UserLinks config={config} labeled /> : null}
           </div>
         </div>
       </footer>
