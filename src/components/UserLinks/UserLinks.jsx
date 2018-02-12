c07a22e8a9c2b62f9cb7470a510dfa6a665cc09a
Diego Hernandez <Diego.Hernandez@ibm.com>
2018-02-12 12:24:49 -0600

    refactor(user-links): Make UserLinks component a functional component

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "carbon-components-react";
 import "./UserLinks.scss";
 
-class UserLinks extends Component {
-  render() {
-    const { labeled } = this.props;
-    const { userLinks } = this.props.config;
-    if (!userLinks) {
-      return null;
-    }
-    return (
-      <div className="user-links">
-        {userLinks.map(link =>
-          (<Link href={link.url} className="user-links__link">
-            <i className={link.iconClassName} /> {labeled ? link.label : ""}
-          </Link>)
-        )}
-      </div>
-    );
+const UserLinks = (props) => {
+  const { labeled } = props;
+  const { userLinks } = props.config;
+  if (!userLinks) {
+    return null;
   }
+  return (
+    <div className="user-links">
+      {userLinks.map(link =>
+        (<Link href={link.url} className="user-links__link">
+          <i className={link.iconClassName} /> {labeled ? link.label : ""}
+        </Link>)
+      )}
+    </div>
+  );
 }
 
 export default UserLinks;
