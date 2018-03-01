b18f3d0b31a3e890150997ba4cdfeec73aca5f8b
Diego Hernandez <Diego.Hernandez@ibm.com>
2018-02-28 22:58:17 -0600

    refactor(about): Remove unnecessary markup

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,17 +8,13 @@ class About extends Component {
 	render() {
     return (
       <Tile className="about">
-        <div className="about__avatar-container">
-          <img
-            src={config.userAvatar}
-            className="about__avatar"
-            alt={config.userName}
-          />
-        </div>
-        <div className="about__content">
-          <p>Spicy jalapeno bacon ipsum dolor amet venison strip steak beef ribs, jowl turducken prosciutto drumstick porchetta buffalo doner biltong tri-tip beef corned beef. Cupim pork chop landjaeger, pork loin tenderloin pork kevin brisket doner drumstick. Landjaeger biltong buffalo shank short ribs. Jowl chicken rump pork loin ham hock, short ribs buffalo pig ball tip ham shank t-bone. Shankle strip steak swine, pancetta kevin corned beef sausage chicken pig pork loin pork t-bone turkey beef ribs.</p>
-          <UserLinks labeled config={config} />
-        </div>
+        <img
+          src={config.userAvatar}
+          className="about__avatar"
+          alt={config.userName}
+        />
+        <p>Spicy jalapeno bacon ipsum dolor amet venison strip steak beef ribs, jowl turducken prosciutto drumstick porchetta buffalo doner biltong tri-tip beef corned beef. Cupim pork chop landjaeger, pork loin tenderloin pork kevin brisket doner drumstick. Landjaeger biltong buffalo shank short ribs. Jowl chicken rump pork loin ham hock, short ribs buffalo pig ball tip ham shank t-bone. Shankle strip steak swine, pancetta kevin corned beef sausage chicken pig pork loin pork t-bone turkey beef ribs.</p>
+        <UserLinks labeled config={config} />
       </Tile>
     );
   }
