90718947fd5da3e811a9c5c33ad8da644f0855a7
Diego Hernandez <Diego.Hernandez@ibm.com>
2018-02-28 22:55:37 -0600

    refactor(about): Update about component styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,20 +7,19 @@ import "./About.scss";
 class About extends Component {
 	render() {
     return (
-      <div className="about-container md-grid mobile-fix">
-        <Tile>
-          <div className="about-wrapper">
-            <img
-              src={config.userAvatar}
-              className="about-img"
-              alt={config.userName}
-            />
-            <p className="about-text md-body-1">Spicy jalapeno bacon ipsum dolor amet venison strip steak beef ribs, jowl turducken prosciutto drumstick porchetta buffalo doner biltong tri-tip beef corned beef. Cupim pork chop landjaeger, pork loin tenderloin pork kevin brisket doner drumstick. Landjaeger biltong buffalo shank short ribs. Jowl chicken rump pork loin ham hock, short ribs buffalo pig ball tip ham shank t-bone. Shankle strip steak swine, pancetta kevin corned beef sausage chicken pig pork loin pork t-bone turkey beef ribs.
-            </p>
-          </div>
+      <Tile className="about">
+        <div className="about__avatar-container">
+          <img
+            src={config.userAvatar}
+            className="about__avatar"
+            alt={config.userName}
+          />
+        </div>
+        <div className="about__content">
+          <p>Spicy jalapeno bacon ipsum dolor amet venison strip steak beef ribs, jowl turducken prosciutto drumstick porchetta buffalo doner biltong tri-tip beef corned beef. Cupim pork chop landjaeger, pork loin tenderloin pork kevin brisket doner drumstick. Landjaeger biltong buffalo shank short ribs. Jowl chicken rump pork loin ham hock, short ribs buffalo pig ball tip ham shank t-bone. Shankle strip steak swine, pancetta kevin corned beef sausage chicken pig pork loin pork t-bone turkey beef ribs.</p>
           <UserLinks labeled config={config} />
-        </Tile>
-      </div>
+        </div>
+      </Tile>
     );
   }
 }
